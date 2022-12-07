3b95b31e33e801a5dd9d61248624ef501cb00633
Mael Normant <mael@normant.name>
2022-12-07 17:58:04 -0500

    ajout de fichiers

--- calorie-counting.py
+++ calorie-counting.py
@@ -6,12 +6,12 @@ with open("input.txt") as file:
     for line in file:
         tables.append(line)
 for i, calorie in enumerate(tables):
-    if calorie != "\n" and calorie != "end\n":
+    if calorie != "\n":
         calories += int(calorie)
-    if tables[i+1] == "\n":
-        sums.append(calories)
-        calories = 0
-    if tables[i+1] == "end\n":break
+    if calorie != tables[-1]:
+        if tables[i+1] == "\n":
+            sums.append(calories)
+            calories = 0
 sums.sort()
 print("the most calories :")
 print(sums[-1])
