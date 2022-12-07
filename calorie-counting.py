b1e28d96aa712721c5845007ee750220ea7914ff
Mael Normant <mael@normant.name>
2022-12-07 17:54:46 -0500

    ajout de fichiers

--- calorie-counting.py
+++ calorie-counting.py
@@ -6,15 +6,16 @@ sums = []
 
 with open('input.csv') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
-    for line in csv_file:
+    for line in reader:
         tables.append(line)
 for i, calorie in enumerate(tables):
-    if calorie != "\n" and calorie != "end\n":
-        calories += int(calorie)
-    if tables[i+1] == "\n":
-        sums.append(calories)
-        calories = 0
-    if tables[i+1] == "end\n":break
+    print(calorie)
+    if len(calorie) == 1:
+        calories += int(calorie[0])
+    if calorie != tables[-1]:
+        if len(tables[i+1]) == 0:
+            sums.append(calories)
+            calories = 0
 sums.sort()
 print("the most calories :")
 print(sums[-1])
