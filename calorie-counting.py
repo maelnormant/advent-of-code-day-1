0223786a191ccdaf81a8187d3c40c79084d31dbd
Mael Normant <mael@normant.name>
2022-12-07 21:16:05 -0500

    ajout des fichiers

--- calorie-counting.py
+++ calorie-counting.py
@@ -1,23 +1,13 @@
-import csv
+def calorie_counting(calories):
+    return sum(map(int, calories.split("\n")))
 
-tables = []
-calories = 0
-sums = []
-
-with open('input.csv') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    for line in reader:
-        tables.append(line)
-for i, calorie in enumerate(tables):
-    print(calorie)
-    if len(calorie) == 1:
-        calories += int(calorie[0])
-    if calorie != tables[-1]:
-        if len(tables[i+1]) == 0:
-            sums.append(calories)
-            calories = 0
-sums.sort()
+with open("input.txt") as file:
+    map_object = map(calorie_counting, file.read().split("\n\n"))
+    list_object = list(map_object)
+    list_object.sort()
+    l = len(list_object)
+    
 print("the most calories :")
-print(sums[-1])
+print(list_object[-1])
 print("the sum of the three most calories :")
-print(sums[-1]+sums[-2]+sums[-3])
+print(sum(list_object[l-3:l]))
